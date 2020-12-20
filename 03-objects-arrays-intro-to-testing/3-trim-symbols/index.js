59b7d66e14faedd6dfeea8b5bcdaff717f20d452
itsanti <kurov.aleksandr+github@gmail.com>
2020-12-20 20:14:40 +0300

    решеие обязательных задач 3 урока

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,26 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  } else if (size === 0 || !size) {
+    return '';
+  }
 
+  const result = [];
+  let counter = 0;
+  let current = null;
+
+  for (const char of string) {
+    if (current !== char) {
+      current = char;
+      counter = 0;
+    }
+    if (counter < size) {
+      counter++;
+      result.push(char);
+    }
+  }
+
+  return result.join('');
 }
